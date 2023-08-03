3c17cb08315c54507ca56c768ca04478382a8c0d
severinzhong <severinzhong@gmail.com>
2023-08-03 21:08:16 +0800

    try resigter ws

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import React, { useState ,useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [clickerCount , setClickerCount] = useState(0) ;
-
+  
   useEffect(()=>{
-
-    let deferredPrompt;
-    const addBtn = document.querySelector(".add-button");
-    addBtn.style.display = "none";
-
-    window.addEventListener("beforeinstallprompt", (e) => {
-      // 防止 Chrome 67 及更早版本自动显示安装提示
-      e.preventDefault();
-      // 稍后再触发此事件
-      deferredPrompt = e;
-      // 更新 UI 以提醒用户可以将 App 安装到桌面
-      addBtn.style.display = "block";
-    
-      addBtn.addEventListener("click", (e) => {
-        // 隐藏显示 A2HS 按钮的界面
-        // addBtn.style.display = "none";
-        // 显示安装提示
-        deferredPrompt.prompt();
-        // 等待用户反馈
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-          deferredPrompt = null;
-        });
-      });
+    let installPrompt = null;
+    const installButton = document.querySelector("#install");
+  
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      installPrompt = event;
+      installButton.removeAttribute("hidden");
+    });
+  
+    installButton.addEventListener("click", async () => {
+      if (!installPrompt) {
+        return;
+      }
+      const result = await installPrompt.prompt();
+      console.log(`Install prompt was: ${result.outcome}`);
+      installPrompt = null;
+      installButton.setAttribute("hidden", "");
     });
   },[])
+
 
   return (
     <div className="App">
@@ -55,7 +45,7 @@ function App() {
         >
           Learn React
         </a>
-        <button className='add-button'>TEST</button>
+        <button id="install" hidden>Install</button>
       </header>
     </div>
   );
